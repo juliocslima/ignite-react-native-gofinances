aa6581b279d44d8beca0fede9ac9a68d46d6ad38
Julio Lima <lima.julio.cs@gmail.com>
2021-12-20 18:02:26 -0300

    Card component created

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+import { 
+  Container,
+  Header,
+  Title,
+  Icon,
+  Content,
+  Amount,
+  LastTransaction,
+} from './styles';
+
+interface CardProps {
+  title: string;
+  amount: string;
+  lastTransaction: string;
+  type: 'income' | 'outcome' | 'total';
+}
+
+const icon = {
+  income: 'arrow-down-circle',
+  outcome: 'arrow-up-circle',
+  total: 'dollar-sign',
+}
+
+export function Card({ type, title, amount, lastTransaction}: CardProps) {
+
+  return(
+    <Container type={type}>
+      <Header>
+        <Title type={type}>
+          {title}
+        </Title>
+        <Icon 
+          name={icon[type]} 
+          type={type} 
+        />
+      </Header>
+
+      <Content>
+        <Amount type={type}>
+          {amount}
+        </Amount>
+        <LastTransaction type={type}>
+          {lastTransaction}
+        </LastTransaction>
+      </Content>
+    </Container>
+  );
+}
--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,12 +1,57 @@
 import React from 'react';
-
-import { Container, Title } from './styles';
+import { HighlightCard } from '../../components/HighlightCard';
+import { Card } from '../../components/Card';
+import { 
+  Container, 
+  Header,
+  HighlightCards,
+  Photo,
+  User,
+  UserContainer,
+  UserInfo,
+  UserGretting,
+  UserName,
+  Icon,
+} from './styles';
 
 export function Dashboard() {
 
   return(
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserContainer>
+          <UserInfo>
+            <Photo source={{ uri: 'https://avatars.githubusercontent.com/u/86537737?v=4'}}/>
+            <User>
+              <UserGretting>Olá</UserGretting>
+              <UserName>Julio Lima</UserName>
+            </User>
+          </UserInfo>
+
+          <Icon name="power"/>
+        </UserContainer>
+      </Header>
+
+      <HighlightCards>
+        <Card 
+          type="income"
+          title="Entradas"
+          amount="R$ 17.400,00"
+          lastTransaction="Última entrada dia 13 de abril"
+        />
+        <Card 
+          type="outcome"
+          title="Saídas"
+          amount="R$ 1.259,00"
+          lastTransaction="Última saída dia 03 de abril"
+        />
+        <Card 
+          type="total"
+          title="Total"
+          amount="R$ 16.141,00"
+          lastTransaction="01 à 16 de abril"
+        />
+      </HighlightCards>
     </Container>
   );
 }
